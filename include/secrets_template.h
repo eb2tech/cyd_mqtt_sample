498b8c74bba899b86a9e85e737cdb1eb51efb80f
Edward Blakes <eblakes@eb2technologies.com>
2025-08-02 00:29:32 -0500

    Add secrets_template.h for WiFi and MQTT credentials configuration

---
+++ include/secrets_template.h
@@ -0,0 +1,8 @@
+// Rename to secrets.h and fill in your WiFi and MQTT credentials
+#pragma once
+
+#define WIFI_SSID "Your Wifi SSID"
+#define WIFI_PASSWORD "Your Wifi Password"
+#define MQTT_USER "Your MQTT Username"
+#define MQTT_PASSWORD "Your MQTT Password"
+#define MQTT_BROKER_IP "Your MQTT Broker IP"
